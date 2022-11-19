a6b0950387c8b62b9d9ae47357a3433e999406e5
Kamil Grondys <kamil.grondys@codecool.com>
2022-11-19 14:55:01 +0100

    adding first two columns

--- static/api_wars.js
+++ static/api_wars.js
@@ -1,1 +1,23 @@
-alert('hello, world')
+function getPlanetsDetails() {
+
+    fetch("https://swapi.dev/api/planets/").then((resp)=>resp.json()).then((data)=>{
+
+        data.results.forEach(planet=>{
+            let tbody = document.getElementById("planets");
+            let row = document.createElement('tr');
+            let nameCol = document.createElement('td');
+            nameCol.innerHTML = planet.name;
+            let diameterCol = document.createElement('td');
+            diameterCol.innerHTML = planet.diameter;
+
+            //...
+
+            row.appendChild(nameCol);
+            row.appendChild(diameterCol);
+            tbody.append(row);
+        }
+        )
+
+    }
+    )
+}
