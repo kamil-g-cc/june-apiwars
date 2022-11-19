a5eaca715b47bc0beb21400228bb953c3af27db9
Karola-ING <108148869+Karola-ING@users.noreply.github.com>
2022-11-19 15:34:28 +0100

    getting all data for table

--- static/api_wars.js
+++ static/api_wars.js
@@ -1,23 +1,33 @@
 function getPlanetsDetails() {
-
     fetch("https://swapi.dev/api/planets/").then((resp)=>resp.json()).then((data)=>{
 
         data.results.forEach(planet=>{
-            let tbody = document.getElementById("planets");
+            let tbody = document.getElementById('planets');
             let row = document.createElement('tr');
-            let nameCol = document.createElement('td');
+            let nameCol = document.createElement('td')
             nameCol.innerHTML = planet.name;
             let diameterCol = document.createElement('td');
             diameterCol.innerHTML = planet.diameter;
+            let climateCol = document.createElement('td');
+            climateCol.innerHTML = planet.climate;
+            let terrainCol = document.createElement('td');
+            terrainCol.innerHTML = planet.terrain;
+            let surfaceWaterCol = document.createElement('td');
+            surfaceWaterCol.innerHTML = planet.surface_water;
+            let populationCol = document.createElement('td');
+            populationCol.innerHTML = planet.population;
 
             //...
 
-            row.appendChild(nameCol);
-            row.appendChild(diameterCol);
+            row.append(nameCol);
+            row.append(diameterCol);
+            row.append(climateCol);
+            row.append(terrainCol);
+            row.append(surfaceWaterCol);
+            row.append(populationCol);
             tbody.append(row);
         }
         )
-
     }
     )
 }
